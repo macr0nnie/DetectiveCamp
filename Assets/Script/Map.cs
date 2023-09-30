ec88d8bc2a797e51ece328f87b5b638234376578
macr0nnie <macr0nnieasmr@gmail.com>
2023-09-30 11:47:23 -0400

    add dialougemanger

--- Assets/Script/Map.cs
+++ Assets/Script/Map.cs
@@ -2,28 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class Map : MonoBehaviour
 {
     //this script is attached to the map UI 
     //it opens the map panel and allows players to move to different areas of the map. 
     //the camera will move to different locations using transform
     //location settings
-    public Transform camp; 
-    public Transform forest;
-    public Transform cafe; 
+    public Transform camp;
     public Transform cabin;
-    public Transform lake;
     public GameObject MapPanel;
-
+    
     Transform currentLocation;    
     public GameObject camera; 
 
     void Start()
     {
+        currentLocation = camp;
         //set the location to the center of the camp; 
-
     }
     // Update is called once per frame
     void Update()
@@ -31,19 +26,25 @@ public class Map : MonoBehaviour
         if(currentLocation.transform.position != camera.transform.position){
             camera.transform.position = currentLocation.position;
         }     
+        if(Input.GetKeyDown(KeyCode.M)){
+          EnableMap();
+        }
     }    
     //used to change the location
-    void SetLocation( Transform tp){
+    public void SetLocation( Transform tp){
         currentLocation = tp;
+        Debug.Log(currentLocation);
     }
+    public void EnableMap(){
 
-    void EnableMap(){
-        if(MapPanel.active==  true){
-        MapPanel.SetActive(true);
+        if(MapPanel.activeSelf){
+        MapPanel.SetActive(false);
         }
-         else{
-             MapPanel.SetActive(false);
-         }
+        else{
+             MapPanel.SetActive(true);
+        }
+        
+
     }
 
 
